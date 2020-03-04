d54950dd4b391623c6973578441d998bc9717878
zydplatform <31594456+zydplatform@users.noreply.github.com>
2020-03-04 10:15:55 +0300

    updated contact form

--- views/contactform.php
+++ views/contactform.php
@@ -34,8 +34,12 @@
                <li class="nav-item">
             <a class="nav-link" href="contactform.php">Gallery</a>
           </li>
-           <li class="nav-item">
-            <a class="nav-link" href="../documents/cv-kigandaivan.pdf" target="_blank">Download CV here</a>
+          <li class="nav-item">
+            <a class="nav-link" href="../documents/Rosemary Isoto_Short CV.pdf" target="_blank" >Download CV here</a>
+          </li>
+         
+          <li class="nav-item">
+            <a class="nav-link" href="../documents/A Brief Biography about Dr. Isoto.pdf" target="_blank" >Biography</a>
           </li>
         
         </ul>
